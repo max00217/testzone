715b51b5d9128829ecd5355c4f2851ce81ea4b54
Minauroht_ <makpung00217@gmail.com>
2023-03-06 20:43:01 +0900

    randsnake

--- Lua/LoveEngine/randomsnake/main.lua
+++ Lua/LoveEngine/randomsnake/main.lua
@@ -2,65 +2,84 @@ function love.load()
     -- set initial position and size of the snake
     snake = {
         {x = 350, y = 350},
-        {x = 200, y = 220},
-        {x = 200, y = 240},
-        {x = 200, y = 260},
-        {x = 200, y = 280},
-        {x = 200, y = 220},
-        {x = 200, y = 240},
-        {x = 200, y = 260},
-        {x = 200, y = 220},
-        {x = 200, y = 240},
-        {x = 200, y = 260},
-        {x = 200, y = 220},
-        {x = 200, y = 240},
-        {x = 200, y = 260},
-        {x = 200, y = 220},
-        {x = 200, y = 240},
-        {x = 200, y = 260},
-        {x = 200, y = 220},
-        {x = 200, y = 240},
-        {x = 200, y = 260},
-        {x = 200, y = 220},
-        {x = 200, y = 240},
-        {x = 200, y = 260},
-        {x = 200, y = 220},
-        {x = 200, y = 240},
-        {x = 200, y = 260},
-        {x = 200, y = 220},
-        {x = 200, y = 240},
-        {x = 200, y = 260},
-        {x = 200, y = 220},
-        {x = 200, y = 240},
-        {x = 200, y = 260},
-        {x = 200, y = 220},
-        {x = 200, y = 240},
-        {x = 200, y = 260},
-        {x = 200, y = 220},
-        {x = 200, y = 240},
-        {x = 200, y = 260},
-        {x = 200, y = 220},
-        {x = 200, y = 240},
-        {x = 200, y = 260},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
+        {x = 350, y = 350},
     }
+    directions = {"up", "down", "left", "right"}
 
-    -- set initial direction of the snake
-    direction = "up"
-
-    -- set size of the window
+    direction = directions[love.math.random(1, #directions)]
+    
     love.window.setMode(700, 700)
+    windowwidth = love.graphics.getWidth()
+	windowheight = love.graphics.getHeight()
+    StartTimer = 0
 end
 
 function love.update(dt)
+    StartTimer = StartTimer + dt
+    timer = tonumber(string.format("%.0f", StartTimer))
+    --next_time = next_time + min_dt
+
     -- randomly choose a new direction for the snake
     local new_direction = direction
-    while new_direction == direction do
-        local directions = {"up", "down", "left", "right"}
-        new_direction = directions[love.math.random(1, #directions)]
+    
+        --while new_direction == direction do
+
+    --while timer % 2 == 0 do
+    if timer % 1 == 0 then
+            new_direction = directions[love.math.random(1, #directions)]
+        --end
     end
     direction = new_direction
 
-    -- move the snake in the chosen direction
+    if snake[1].x >= windowwidth then
+        direction = "left"
+    elseif snake[1].x <= 0 then
+        direction = "right"
+    elseif snake[1].y >= windowheight then
+        direction = "up"
+    elseif snake[1].y <= 0 then
+        direction = "down"
+    end
+
     if direction == "up" then
         table.insert(snake, 1, {x = snake[1].x, y = snake[1].y - 5})
     elseif direction == "down" then
@@ -70,14 +89,13 @@ function love.update(dt)
     elseif direction == "right" then
         table.insert(snake, 1, {x = snake[1].x + 5, y = snake[1].y})
     end
-
-    -- remove the tail of the snake to keep its length constant
     table.remove(snake)
 end
 
 function love.draw()
-    -- draw the snake
     for i, segment in ipairs(snake) do
         love.graphics.rectangle("fill", segment.x, segment.y, 5, 5)
     end
+
+    love.graphics.print(timer)
 end

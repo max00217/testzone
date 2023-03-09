886a80f85eb2a9c4d42a7d217f520a4182a435d7
minauroht <makpung00217@gmail.com>
2023-03-09 23:07:09 +0900

    survival bullet

--- Lua/main.lua
+++ Lua/main.lua
@@ -1,139 +1,77 @@
 function love.load()
-	-- Set up the game variables
-	ball = { x = 320, y = 240, dx = 200, dy = 200, radius = 10 }
-	paddles = {
-	  { x = 20, y = 200, dy = 0, width = 10, height = 80 }, -- player 1
-	  { x = 610, y = 200, dy = 0, width = 10, height = 80 }, -- player 2
-	  { x = 320, y = 20, dy = 0, width = 80, height = 10 }, -- player 3
-	  { x = 320, y = 450, dy = 0, width = 80, height = 10 }, -- player 4
+	-- Set initial player position
+	player = {
+	  x = love.graphics.getWidth() / 2 - 25, -- subtract half of player width
+	  y = love.graphics.getHeight() / 2 - 25, -- subtract half of player height
+	  width = 50,
+	  height = 50,
+	  speed = 200
 	}
-	scores = { 0, 0, 0, 0 }
+	
+	-- Set initial bullet position and size
+	bullet = {
+	  x = 0,
+	  y = 0,
+	  radius = 5,
+	  speed = 500,
+	  active = false -- bullet is inactive until fired
+	}
   end
   
   function love.update(dt)
-	-- Update the ball position
-	ball.x = ball.x + ball.dx * dt
-	ball.y = ball.y + ball.dy * dt
-  
-	-- Check for ball collision with the walls and paddles
-	if ball.y < ball.radius or ball.y > love.graphics.getHeight() - ball.radius then
-	  ball.dy = -ball.dy
+	-- Move player with WASD keys
+	if love.keyboard.isDown("w") then
+	  player.y = player.y - player.speed * dt
+	elseif love.keyboard.isDown("s") then
+	  player.y = player.y + player.speed * dt
 	end
-	for i, paddle in ipairs(paddles) do
-	  if checkCollision(ball.x, ball.y, ball.radius, paddle.x, paddle.y, paddle.width, paddle.height) then
-		ball.dx = -ball.dx
-		ball.dy = ball.dy + paddle.dy / 2
+	if love.keyboard.isDown("a") then
+	  player.x = player.x - player.speed * dt
+	elseif love.keyboard.isDown("d") then
+	  player.x = player.x + player.speed * dt
+	end
+	
+	-- Fire bullet when mouse is clicked
+	if love.mouse.isDown(1) then
+	  -- Set bullet position to player position
+	  bullet.x = player.x + player.width / 2 -- add half of player width to center bullet
+	  bullet.y = player.y + player.height / 2 -- add half of player height to center bullet
+	  
+	  -- Calculate direction to mouse cursor
+	  local mouseX, mouseY = love.mouse.getPosition()
+	  local dx = mouseX - bullet.x
+	  local dy = mouseY - bullet.y
+	  local dist = math.sqrt(dx * dx + dy * dy)
+	  
+	  -- Normalize direction and set bullet velocity
+	  bullet.vx = bullet.speed * dx / dist
+	  bullet.vy = bullet.speed * dy / dist
+	  
+	  -- Set bullet as active
+	  bullet.active = true
+	end
+	
+	-- Move bullet when active
+	if bullet.active then
+	  bullet.x = bullet.x + bullet.vx * dt
+	  bullet.y = bullet.y + bullet.vy * dt
+	  
+	  -- Deactivate bullet when it goes off screen
+	  if bullet.x < -bullet.radius or bullet.x > love.graphics.getWidth() + bullet.radius
+	  or bullet.y < -bullet.radius or bullet.y > love.graphics.getHeight() + bullet.radius then
+		bullet.active = false
 	  end
-	end
-  
-	-- Update the paddle positions
-	for i, paddle in ipairs(paddles) do
-	  paddle.y = paddle.y + paddle.dy * dt
-	  if paddle.y < 0 then
-		paddle.y = 0
-	  elseif paddle.y + paddle.height > love.graphics.getHeight() then
-		paddle.y = love.graphics.getHeight() - paddle.height
-	  end
-	end
-  
-	-- Check for paddle collision with the walls
-	for i, paddle in ipairs(paddles) do
-	  if paddle.y < 0 then
-		paddle.y = 0
-	  elseif paddle.y + paddle.height > love.graphics.getHeight() then
-		paddle.y = love.graphics.getHeight() - paddle.height
-	  end
-	end
-  
-	-- Check for ball going out of bounds
-	if ball.x < -ball.radius then
-	  scores[2] = scores[2] + 1
-	  reset()
-	elseif ball.x > love.graphics.getWidth() + ball.radius then
-	  scores[1] = scores[1] + 1
-	  reset()
 	end
   end
   
   function love.draw()
-	-- Draw the ball and paddles
-	love.graphics.circle("fill", ball.x, ball.y, ball.radius)
-	for i, paddle in ipairs(paddles) do
-	  love.graphics.rectangle("fill", paddle.x, paddle.y, paddle.width, paddle.height)
+	-- Draw player rectangle
+	love.graphics.setColor(1, 1, 1)
+	love.graphics.rectangle("fill", player.x, player.y, player.width, player.height)
+	
+	-- Draw bullet circle when active
+	if bullet.active then
+	  love.graphics.setColor(1, 0, 0)
+	  love.graphics.circle("fill", bullet.x, bullet.y, bullet.radius)
 	end
-  
-	-- Draw the scores
-	love.graphics.printf("Player 1: " .. scores[1], 0, 10, 150, "center")
-	love.graphics.printf("Player 2: " .. scores[2], love.graphics.getWidth() - 150, 10, 150, "center")
-	love.graphics.printf("Player 3: " .. scores[3], 0, love.graphics.getHeight() - 30, 150, "center")
-	love.graphics.printf("Player 4: " .. scores[4], love.graphics)
-end
-  function love.keypressed(key)
-		-- Player 1 controls
-	if key == "w" then
-		paddles[1].dy = -200
-	elseif key == "s" then
-		paddles[1].dy = 200
-	end
-		
-		-- Player 2 controls
-	if key == "up" then
-		paddles[2].dy = -200
-	elseif key == "down" then
-		paddles[2].dy = 200
-	end
-		
-		-- Player 3 controls
-	if key == "a" then
-		paddles[3].dy = -200
-	elseif key == "d" then
-		paddles[3].dy = 200
-	end
-		
-		-- Player 4 controls
-	if key == "left" then
-		paddles[4].dy = -200
-	elseif key == "right" then
-		paddles[4].dy = 200
-		end
-	end	
-	function love.keyreleased(key)
-		-- Player 1 controls
-		if key == "w" or key == "s" then
-		paddles[1].dy = 0
-		end
-		
-		-- Player 2 controls
-		if key == "up" or key == "down" then
-		paddles[2].dy = 0
-		end
-		
-		-- Player 3 controls
-		if key == "a" or key == "d" then
-		paddles[3].dy = 0
-		end
-		
-		-- Player 4 controls
-		if key == "left" or key == "right" then
-		paddles[4].dy = 0
-		end
-		end
-		
-	function reset()
-		-- Reset the ball and paddle positions
-		ball.x = love.graphics.getWidth() / 2
-		ball.y = love.graphics.getHeight() / 2
-		ball.dx = 200
-		ball.dy = 200
-		for i, paddle in ipairs(paddles) do
-		paddle.y = love.graphics.getHeight() / 2 - paddle.height / 2
-		paddle.dy = 0
-		end
-		end
-		
-	function checkCollision(x1, y1, r1, x2, y2, w2, h2)
-		-- Check for collision between a circle and a rectangle
-		local dx = x1 - math.max(x2, math.min(x1, x2 + w2))
-		local dy = y1 - math.max(y2, math.min(y1, y2 + h2))
-		return (dx * dx + dy * dy) < (r1 * r1)
-		end
+  end

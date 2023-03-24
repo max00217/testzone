aaaf0c91cac368037f47ee634fcad300b0065008
minauroht <makpung00217@gmail.com>
2023-03-24 10:37:50 +0900

    yay

--- Lua/al/RPS.lua
+++ Lua/al/RPS.lua
@@ -6,20 +6,21 @@ function table.indexOf(t, value)
     end
     return nil
 end
+for i = 1, 3 do
+    io.write("R, P, or S: ")
+    user = io.read()
 
-io.write("R, P, or S: ")
-user = io.read()
+    rps_table = {"R", "P", "S"}
 
-rps_table = {"R", "P", "S"}
+    bot = rps_table[math.random(#rps_table)]
 
-bot = rps_table[math.random(#rps_table)]
+    result = table.indexOf(rps_table, user) - table.indexOf(rps_table, bot)
 
-result = table.indexOf(rps_table, user) - table.indexOf(rps_table, bot)
-
-if result == 0 then
-    print(string.format('%s vs %s  Draw', user, bot))
-elseif result == 1 or result == -2 then
-    print(string.format('%s vs %s  You Wins', user, bot))
-else
-    print(string.format('%s vs %s  You Lose', user, bot))
+    if result == 0 then
+        print(string.format('%s vs %s  Draw', user, bot))
+    elseif result == 1 or result == -2 then
+        print(string.format('%s vs %s  You Wins', user, bot))
+    else
+        print(string.format('%s vs %s  You Lose', user, bot))
+    end
 end

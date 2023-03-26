d75e2c6727d1c58faf41e55b1a7b3c5cfe33b59c
Minauroht <102131045+Minauroht@users.noreply.github.com>
2023-03-26 18:31:55 +0900

    luajit testing

--- Lua/al/break.lua
+++ Lua/al/break.lua
@@ -1,5 +1,5 @@
-local startTime = os.time()
-local endTime = startTime + 5  -- run for 5 seconds
+startTime = os.time()
+endTime = startTime + 5  -- run for 5 seconds
 
 for i = 1, 20000 do
   if os.time() >= endTime then

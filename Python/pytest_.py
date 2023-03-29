65b6ae5bd79b8854ce78b85eb21ab2be468cdf5e
minauroht <makpung00217@gmail.com>
2023-03-29 20:54:46 +0900

    P

--- Python/pytest_.py
+++ Python/pytest_.py
@@ -1,2 +1,12 @@
-import numpy
+import random
 
+user = input(str("R, P or, S: "))
+rps_table = ["R", "P", "S"]
+bot = random.choice(rps_table)
+result = rps_table.index(user) - rps_table.index(bot)
+if result == 0:
+    print(f"{user} vs {bot}  Draw")
+elif result == 1 or result == -2:
+    print(f"{user} vs {bot}  You Win")
+else:
+    print(f"{user} vs {bot}  You Lose")

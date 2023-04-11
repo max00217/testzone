ef4ec8f4318d041d938384ded915166bc2c42857
minauroht <makpung00217@gmail.com>
2023-04-11 20:53:55 +0900

    lua moushigi

--- Lua/Lua.lua
+++ Lua/Lua.lua
@@ -1,1 +1,11 @@
-os.execute("ipconfig /all")
+io.write("Enter a number: ")
+local num = tonumber(io.read())
+
+local i = 1
+
+while i <= num do
+    if num % i == 0 then
+        print(i)
+    end
+    i = i + 1
+end

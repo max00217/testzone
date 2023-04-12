c27e7262624cd7667a2df4e5c49171cab333ac4e
minauroht <makpung00217@gmail.com>
2023-04-12 20:58:08 +0900

    루아 플레이그라운드

--- Lua/Lua.lua
+++ Lua/Lua.lua
@@ -1,11 +1,1 @@
-io.write("Enter a number: ")
-local num = tonumber(io.read())
-
-local i = 1
-
-while i <= num do
-    if num % i == 0 then
-        print(i)
-    end
-    i = i + 1
-end
+print(90929349949999^377)

5bafed69b48966be32a8e52264d60e22f3190d46
minauroht <makpung00217@gmail.com>
2023-04-13 16:25:20 +0900

    metatable

--- Lua/Lua.lua
+++ Lua/Lua.lua
@@ -1,1 +1,32 @@
-print(90929349949999^377)
+mt = {}
+t1 = {}
+t2 = {}
+t3 = {}
+ 
+function mt:new(o)
+        o = o or {}
+        setmetatable(o, self)
+        self.__index = self
+        return o
+end
+ 
+function mt:funcTest()
+        print('mtFunc')
+end
+ 
+function t2:funcTest()
+        print('t2Func')
+end
+ 
+function t3:funcTest()
+        print('t3Func')
+end
+ 
+t1 = mt:new(t1)
+t1.funcTest()
+ 
+t2 = mt:new(t2)
+t2.funcTest()
+ 
+t3 = t2:new(t3)
+t3.funcTest()

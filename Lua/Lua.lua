992a6cea2785231d3f64e19fa1765b8d8a2d98bf
minauroht <makpung00217@gmail.com>
2023-04-16 00:47:59 +0900

    wut

--- Lua/Lua.lua
+++ Lua/Lua.lua
@@ -1,3 +1,14 @@
-for i = 1, 50000 do
-  print(2^i)
-end
+Person = {
+  name = "John",
+  age = 30
+}
+
+meta = {
+  __call = function(table, ...)
+    print("Calling Person with arguments:", ...)
+  end
+}
+
+setmetatable(Person, meta)
+
+Person("arg1", "arg2") -- "Calling Person with arguments: arg1 arg2"

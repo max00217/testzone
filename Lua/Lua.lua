12dd8269ebbba89e630dbcc03506de6b6bded9f8
minauroht <makpung00217@gmail.com>
2023-04-15 17:37:00 +0900

    eektta

--- Lua/Lua.lua
+++ Lua/Lua.lua
@@ -1,32 +1,3 @@
-local myTable = {}
-
-local mt = {
-  __index = function(table, key)
-    if key == "greeting" then
-      return "Hello!"
-    elseif key == "name" then
-      return "John"
-    else
-      return nil
-    end
-  end,
-
-  __newindex = function(table, key, value)
-    if key == "age" then
-      print("Setting age to:", value)
-      rawset(table, key, value)
-    else
-      print("Invalid key:", key)
-    end
-  end
-}
-
-setmetatable(myTable, mt)
-
-print(myTable.greeting)
-print(myTable.name)
-print(myTable.age)
-
-
-myTable.age = 30
-myTable.gender = "Male"
+for i = 1, 500000 do
+  print(2^i)
+end
---
+++ Python/python.py
@@ -0,0 +1,2 @@
+for i in range(500000):
+  print(2**i)

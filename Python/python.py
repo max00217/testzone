219fe9b65a317c0b2816509d4937ea87904da74d
minauroht <makpung00217@gmail.com>
2023-04-16 22:03:24 +0900

    Lupa test

--- Python/python.py
+++ Python/python.py
@@ -1,2 +1,17 @@
-for i in range(50000):
-  print(2**i)
+import lupa
+from lupa import LuaRuntime
+
+lua = LuaRuntime(unpack_returned_tuples=True)
+
+print(lua.eval('1+1'))
+
+lua_func = lua.eval('function(f, n) return f(n) end')
+
+def py_add1(n): 
+  return n+1
+
+print(lua_func(py_add1, 2))
+
+print(lua.eval('python.eval(" 2 ** 2 ")') == 4)
+
+print(lua.eval('python.builtins.str(4)') == '4')

574bc932ef055d02a73cdda9cdba6fdd2ee6b19f
Minauroht <102131045+Minauroht@users.noreply.github.com>
2023-04-24 23:28:14 +0900

    Baek

--- Lua/Lua.lua
+++ Lua/Lua.lua
@@ -1,38 +1,31 @@
-s = {
-    "  *  ",
-    " * * ",
-    "*****"
-}
+local str = io.read()
 
-m = {}
-
-for i = 1, 3500 do
-    m[i] = {}
-    for j = 1, 6500 do
-        m[i][j] = " "
+function CMS(s)
+    local len = #s
+    local count_a = 0
+    for i = 1, len do
+        if s:sub(i, i) == 'a' then
+            count_a = count_a + 1
+        end
     end
-end
-
-function solve(n, y, x)
-    if n == 1 then
-        for i = 1, 3 do
-            for j = 1, 5 do
-                m[y+i-1][x+j-1] = s[i]:sub(j,j)
+    local q = len
+    for i = 1, len do
+        local count = count_a
+        local t = 0
+        for j = i, i + len - 1 do
+            if count == 0 then
+                break
+            end
+            if s:sub(j % len + 1, j % len + 1) == 'b' then
+                t = t + 1
+                count = count - 1
+            else
+                count = count - 1
             end
         end
-        return
+        q = math.min(q, t)
     end
-    solve(math.floor(n/2), y, x+3*(math.floor(n/2)))
-    solve(math.floor(n/2), y+3*(math.floor(n/2)), x)
-    solve(math.floor(n/2), y+3*(math.floor(n/2)), x+3*n)
+    return q
 end
 
-n = io.read("*n")
-solve(math.floor(n/3), 1, 1)
-
-for i = 1, n do
-    for j = 1, 2*n-1 do
-        io.write(m[i][j] == "*" and "*" or " ")
-    end
-    io.write("\n")
-end
+print(CMS(str))

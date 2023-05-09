0b118c5e497d3b1ba715d43938322c481bc71ca7
Minauroht <102131045+Minauroht@users.noreply.github.com>
2023-05-10 00:05:33 +0900

    html file relative path set

--- Lua/Luvit_web/server.lua
+++ Lua/Luvit_web/server.lua
@@ -1,31 +1,31 @@
-local http = require('http')
-local fs = require('fs')
+local http = require("http")
+local fs = require("fs")
 
 local script_path = debug.getinfo(1).source:sub(2):match("(.*[/\\])")
 local index_file_path = script_path .. "index.html"
 
 -- Create a server object
 local server = http.createServer(function (req, res)
-  if req.url == '/' then
+  if req.url == "/" then
     -- Read the contents of index.html file
     fs.readFile(index_file_path, function (err, data)
       if err then
-        -- Handle error if the file can't be read
-        res:setHeader('Content-Type', 'text/plain')
-        res:setHeader('Status', '500 Internal Server Error')
-        res:finish('Error: ' .. err .. '\n')
+        -- Handle error if the file can"t be read
+        res:setHeader("Content-Type", "text/plain")
+        res:setHeader("Status", "500 Internal Server Error")
+        res:finish("Error: " .. err .. "\n")
       else
         -- Send the contents of the file as the response body
-        res:setHeader('Content-Type', 'text/html')
-        res:setHeader('Status', '200 OK')
+        res:setHeader("Content-Type", "text/html")
+        res:setHeader("Status", "200 OK")
         res:finish(data)
       end
     end)
   else
     -- Handle all other routes
-    res:setHeader('Content-Type', 'text/plain')
-    res:setHeader('Status', '404 Not Found')
-    res:finish('Error: Page not found\n')
+    res:setHeader("Content-Type", "text/plain")
+    res:setHeader("Status", "404 Not Found")
+    res:finish("Error: Page not found\n")
   end
 end)
 

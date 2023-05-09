6318e2fd16305a7157a831c19101ffa6207b8965
Minauroht <102131045+Minauroht@users.noreply.github.com>
2023-05-10 00:15:49 +0900

    Created web pages and hyperlinks using Luvit

--- Lua/Luvit_web/server.lua
+++ Lua/Luvit_web/server.lua
@@ -1,35 +1,34 @@
-local http = require("http")
-local fs = require("fs")
+local http = require('http')
+local fs = require('fs')
 
 local script_path = debug.getinfo(1).source:sub(2):match("(.*[/\\])")
-local index_file_path = script_path .. "index.html"
 
--- Create a server object
+local function processHtml(file_path, req, res)
+  fs.readFile(file_path, function (err, data)
+    if err then
+      res:setHeader('Content-Type', 'text/plain')
+      res:setHeader('Status', '500 Internal Server Error')
+      res:finish('Error: ' .. err .. '\n')
+    else
+      res:setHeader('Content-Type', 'text/html')
+      res:setHeader('Status', '200 OK')
+      res:finish(data)
+    end
+  end)
+end
+
 local server = http.createServer(function (req, res)
-  if req.url == "/" then
-    -- Read the contents of index.html file
-    fs.readFile(index_file_path, function (err, data)
-      if err then
-        -- Handle error if the file can"t be read
-        res:setHeader("Content-Type", "text/plain")
-        res:setHeader("Status", "500 Internal Server Error")
-        res:finish("Error: " .. err .. "\n")
-      else
-        -- Send the contents of the file as the response body
-        res:setHeader("Content-Type", "text/html")
-        res:setHeader("Status", "200 OK")
-        res:finish(data)
-      end
-    end)
+  if req.url == '/' then
+    processHtml(script_path .. "index.html", req, res)
+  elseif req.url == '/second.html' then
+    processHtml(script_path .. "second.html", req, res)
   else
-    -- Handle all other routes
-    res:setHeader("Content-Type", "text/plain")
-    res:setHeader("Status", "404 Not Found")
-    res:finish("Error: Page not found\n")
+    res:setHeader('Content-Type', 'text/plain')
+    res:setHeader('Status', '404 Not Found')
+    res:finish('Error: Page not found\n')
   end
 end)
 
--- Start the server and listen for incoming connections on port 8080
 server:listen(8080, function ()
   print("Server listening at http://localhost:8080/")
 end)

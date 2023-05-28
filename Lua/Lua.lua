4ff3f9deb196834f2a2c0774cb11fa30cf287c0c
Minauroht <102131045+Minauroht@users.noreply.github.com>
2023-05-28 17:34:54 +0900

    Bit Cal

--- Lua/Lua.lua
+++ Lua/Lua.lua
@@ -1,6 +1,5 @@
-a = type
-b = print
-c = function() b(a(c)) end
-
-b(a(b))
-c()
+-- a << b는 a의 비트를 b만큼 왼쪽으로 이동시킵니다.
+local a = 8 -- 00001000 (2진수)
+local b = 2
+local result = a ~ b -- 00100000 (2진수)
+print(result) -- 32 (10진수)

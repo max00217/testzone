4533c45090035caa3bd59a8fa2171f65951dc9a2
minauroht <makpung00217@gmail.com>
2023-06-09 16:28:38 +0900

    tictactoe

--- Lua/LOVE/Tic-Tac-Toe/main.lua
+++ Lua/LOVE/Tic-Tac-Toe/main.lua
@@ -2,9 +2,10 @@ local gridSize = 3
 local cellSize = 190
 local grid = {}
 local clicktime = 0
+local WhoWin = ""
 
 function love.load()
-    -- Create the grid
+    love.window.setMode(600, 600)
     for row = 1, gridSize do
         grid[row] = {}
         for col = 1, gridSize do
@@ -14,27 +15,26 @@ function love.load()
 end
 
 function love.update(dt)
-        if
-        grid[1][1] == grid[1][2] == grid[1][3] == 1 or
-        grid[2][1] == grid[2][2] == grid[2][3] == 1 or
-        grid[3][1] == grid[3][2] == grid[3][3] == 1 or
-        grid[1][1] == grid[2][1] == grid[3][1] == 1 or
-        grid[2][1] == grid[2][2] == grid[3][2] == 1 or
-        grid[3][1] == grid[3][2] == grid[3][3] == 1 or
-        grid[1][1] == grid[2][2] == grid[3][3] == 1 or
-        grid[1][3] == grid[2][2] == grid[3][1] == 1 then
-            love.graphics.print("O Wins", 50, 50)
-        elseif
-        grid[1][1] == grid[1][2] == grid[1][3] == 2 or
-        grid[2][1] == grid[2][2] == grid[2][3] == 2 or
-        grid[3][1] == grid[3][2] == grid[3][3] == 2 or
-        grid[1][1] == grid[2][1] == grid[3][1] == 2 or
-        grid[2][1] == grid[2][2] == grid[3][2] == 2 or
-        grid[3][1] == grid[3][2] == grid[3][3] == 2 or
-        grid[1][1] == grid[2][2] == grid[3][3] == 2 or
-        grid[1][3] == grid[2][2] == grid[3][1] == 2 then
-            love.graphics.print("X Wins", 50, 50)
+    WinCheck()
+    local Draw = true
+    if not WinCheck() then
+        for i = 1, 3 do
+            for j = 1, 3 do
+                if grid[i][j] == 0 then
+                    Draw = false
+                    break
+                end
+            end
+            if not Draw then
+                break
+            end
         end
+    else
+        Draw = false
+    end
+    if Draw then
+        WhoWin = "Draw"
+    end
 end
 
 function love.draw()
@@ -51,6 +51,7 @@ function love.draw()
             end
         end
     end
+    love.graphics.print(WhoWin, 500, 50)
 end
 
 function love.mousepressed(x, y, button)
@@ -66,5 +67,29 @@ function love.mousepressed(x, y, button)
                 grid[clickedRow][clickedColumn] = 2
             end
         end
+    end
+end
+
+function WinCheck()
+    if
+    (grid[1][1] == 1 and grid[1][2] == 1 and grid[1][3] == 1) or
+    (grid[2][1] == 1 and grid[2][2] == 1 and grid[2][3] == 1) or
+    (grid[3][1] == 1 and grid[3][2] == 1 and grid[3][3] == 1) or
+    (grid[1][1] == 1 and grid[2][1] == 1 and grid[3][1] == 1) or
+    (grid[1][2] == 1 and grid[2][2] == 1 and grid[3][2] == 1) or
+    (grid[1][3] == 1 and grid[2][3] == 1 and grid[3][3] == 1) or
+    (grid[1][1] == 1 and grid[2][2] == 1 and grid[3][3] == 1) or
+    (grid[1][3] == 1 and grid[2][2] == 1 and grid[3][1] == 1) then
+        WhoWin = "O Wins"
+    elseif
+    (grid[1][1] == 2 and grid[1][2] == 2 and grid[1][3] == 2) or
+    (grid[2][1] == 2 and grid[2][2] == 2 and grid[2][3] == 2) or
+    (grid[3][1] == 2 and grid[3][2] == 2 and grid[3][3] == 2) or
+    (grid[1][1] == 2 and grid[2][1] == 2 and grid[3][1] == 2) or
+    (grid[1][2] == 2 and grid[2][2] == 2 and grid[3][2] == 2) or
+    (grid[1][3] == 2 and grid[2][3] == 2 and grid[3][3] == 2) or
+    (grid[1][1] == 2 and grid[2][2] == 2 and grid[3][3] == 2) or
+    (grid[1][3] == 2 and grid[2][2] == 2 and grid[3][1] == 2) then
+        WhoWin = "X Wins"
     end
 end

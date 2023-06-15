9b65ad5da98172ed004b5a7908986d158a14735d
Minauroht <102131045+Minauroht@users.noreply.github.com>
2023-06-15 22:09:44 +0900

    Torch Get out

--- Lua/modules/torch7/build/lib/luaT/cmake-exports/luaTConfig.cmake
+++
@@ -1,9 +0,0 @@
-# Find the luaT includes and library
-#
-# LUAT_INCLUDE_DIR -- where to find the includes
-# LUAT_LIBRARIES -- list of libraries to link against
-# LUAT_FOUND -- set to 1 if found
-
-SET(LUAT_FOUND 1)
-SET(LUAT_INCLUDE_DIR "C:/Users/makpu/Downloads/proj/project/Lua/.modules/torch7")
-SET(LUAT_LIBRARIES "C:/Users/makpu/Downloads/proj/project/Lua/.modules/torch7/luaT.dll")

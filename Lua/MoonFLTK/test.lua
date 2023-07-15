3b512cdbea923c2521d8776cc197cf8924a6d0d3
max00217 <makpung00217@gmail.com>
2023-07-15 23:16:42 +0900

    MoonFLTK

--- Lua/MoonFLTK/test.lua
+++ Lua/MoonFLTK/test.lua
@@ -1,7 +1,7 @@
 fl = require("moonfltk")
 
-win = fl.window(340, 180, arg[0])
-box = fl.box(20, 40, 300, 100, "Hello, World!");
+win = fl.window(1280, 960, arg[0])
+box = fl.box(640, 480, 300, 100, "MoonflTK Test + 저기 한국어 지원 하십니까?, 아 대굴빡이");
 box:box('up box')
 box:labelfont(fl.BOLD + fl.ITALIC)
 box:labelsize(36)

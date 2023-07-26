b820e903da49d19b490b8fe08e083d89951146ac
max00217 <makpung00217@gmail.com>
2023-07-26 17:17:24 +0900

    JS Fizzbuzz/99bottle of beer

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -1,1 +1,33 @@
-console.log("Hello, World!")
+function Fizzbuzz() {
+    for (var i = 1; i <= 100; i++) {
+        if (i % 15 == 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 == 0) {
+            console.log("Fizz");
+        } else if (i % 5 == 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i + "");
+        }
+    }
+}
+
+Fizzbuzz();
+
+var Bottle = 99;
+function Bottle99() {
+    while (Bottle > -1) {
+        if (Bottle > 1) {
+            console.log(Bottle + " bottles of beer on the wall, " + Bottle + " bottles of beer.");
+            Bottle -= 1;
+            console.log("Take one down and pass it around, " + Bottle + " bottles of beer on the wall.\n");
+        } else if (Bottle >= 1) {
+            console.log(Bottle + " bottle of beer on the wall, " + Bottle + " bottle of beer.");
+            Bottle -= 1;
+            console.log("Take one down and pass it around, No more bottle of beer on the wall.\n");
+        } else if (Bottle == 0) {
+            console.log("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.");
+            Bottle -= 1;
+        }
+    }
+}
